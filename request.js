c23c67c00d100c285ca92108b83943c1a13b2208
Rodrigofel <56368255+Rodrigofel@users.noreply.github.com>
2022-09-12 14:38:40 -0300

    1 file modified

--- request.js
+++ request.js
@@ -3,5 +3,5 @@ export async function asyncOperation() {
     `https://lh4j4sdpmljdcrcr34gqc6ddni0qvugb.lambda-url.us-east-1.on.aws/?operation=div&a=30&b=40`
   );
   const json = await response.json();
-  console.log(json.result);
+  //console.log(json.result);
 }
